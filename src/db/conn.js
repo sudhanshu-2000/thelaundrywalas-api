a0bf74f18c35ea8f7f411079ae749d44d9dcc2aa
sudhanshu-2000 <developercreazy@gmail.com>
2024-05-23 22:36:35 +0530

    edit

--- src/db/conn.js
+++ src/db/conn.js
@@ -4,7 +4,7 @@ const con = mysql.createPool({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "admin",
+  password: "",
   database: "the_laundry_walas",
   multipleStatements:true
 });
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ const app = express();
 const admin = require('./admin-api-folder/admin');
 const user = require('./user-api-folder/user');
 app.use("/image",express.static('image'));
+PORT = 5498;
 
 app.use('/admin', admin);
 app.use('/user', user); 
 
-const listener = app.listen(process.env.PORT || 4500, () => {
+const listener = app.listen(PORT || 4500, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
